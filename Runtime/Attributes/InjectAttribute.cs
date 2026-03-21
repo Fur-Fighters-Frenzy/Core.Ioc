5302028ac2b512e33bd8d8f78643016342341883
Validosik <69popovvlad@gmail.com>
2026-03-21 13:03:33 -0700

    Feat(ioc): expande `Inject` attribute

--- Runtime/Attributes/InjectAttribute.cs
+++ Runtime/Attributes/InjectAttribute.cs
@@ -2,11 +2,21 @@ using System;
 
 namespace Validosik.Core.Ioc.Attributes
 {
-    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false)]
-    public class InjectAttribute : Attribute
+    [AttributeUsage(
+        AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Constructor | AttributeTargets.Method,
+        AllowMultiple = false,
+        Inherited = true)]
+    public sealed class InjectAttribute : Attribute
     {
+        public string Guid { get; }
+
+        public InjectAttribute()
+        {
+        }
+
         public InjectAttribute(string guid)
         {
+            Guid = guid;
         }
     }
 }
